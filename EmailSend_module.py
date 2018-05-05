c59e019eb8ccbe6092d1c400c0f0fb118478910e
kimjinu <kimjinu5555@gmail.com>
2018-05-05 23:48:41 +0900

    업그래이드 중...
    모듈 구조화

--- EmailSend_module.py
+++ EmailSend_module.py
@@ -1,6 +1,25 @@
 import smtplib
 from email.mime.text import MIMEText
 
+class mail:
+    def __init__(self, receiverID, title, contents):
+        self.email = MIMEText(contents)
+        self.email['Subject'] = title
+        self.email['To'] = receiverID
+
+    def send(self, sender, receiverID):
+        sender.smtp.sendmail(sender.id, receiverID, self.email.as_string())
+        sender.smtp.quit()
+
+class sender:
+    def __init__(self, id, password, provider):
+        self.id = id
+        self.smtp = smtplib.SMTP('smtp.' + provider, 587)
+        self.smtp.ehlo()
+        self.smtp.starttls()
+        self.smtp.login(id, password)
+
+'''
 def createDictionary(sender_ID, sender_PW, sender_Provider, receiver_ID, send_contents, send_title):
     dic = {'send_ID': sender_ID, 'send_PW': sender_PW, 'send_Pro': sender_Provider, 'receive_ID': receiver_ID, 'send_cont': send_contents, 'send_title': send_title}
     return dic
@@ -24,3 +43,4 @@ def run(sender_ID, sender_PW, sender_Provider, receiver_ID, send_contents, send_
     msg = setMsg(info_dic['receive_ID'], info_dic['send_title'], info_dic['send_cont'])
     smtp.sendmail(info_dic['send_ID'], info_dic['receive_ID'], msg.as_string())
     smtp.quit()
+'''

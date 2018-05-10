f2ef2a1abc7d81f34c72c57bf06a20030e1d969d
kimjinu <kimjinu5555@gmail.com>
2018-05-10 20:55:10 +0900

    ver 1.0

--- EmailSend_module.py
+++ EmailSend_module.py
@@ -1,26 +1,21 @@
 import smtplib
 from email.mime.text import MIMEText
 
-def createDictionary(sender_ID, sender_PW, sender_Provider, receiver_ID, send_contents, send_title):
-    dic = {'send_ID': sender_ID, 'send_PW': sender_PW, 'send_Pro': sender_Provider, 'receive_ID': receiver_ID, 'send_cont': send_contents, 'send_title': send_title}
-    return dic
+class mail:
+    def __init__(self, receiverID, title, contents):
+        self.receiverID = receiverID
+        self.email = MIMEText(contents)
+        self.email['Subject'] = title
+        self.email['To'] = receiverID
 
-def createSMTP(ID, PW, provider):
-    smtp = smtp = smtplib.SMTP('smtp.'+provider, 587)
-    smtp.ehlo()
-    smtp.starttls()
-    smtp.login(ID, PW)
-    return smtp
+    def send(self, sender):
+        sender.smtp.sendmail(sender.id, self.receiverID, self.email.as_string())
+        sender.smtp.quit()
 
-def setMsg(ID, title, contents):
-    msg = MIMEText(contents)
-    msg['Subject'] = title
-    msg['To'] = ID
-    return msg
-
-def run(sender_ID, sender_PW, sender_Provider, receiver_ID, send_contents, send_title):
-    info_dic = createDictionary(sender_ID, sender_PW, sender_Provider, receiver_ID, send_contents, send_title)
-    smtp = createSMTP(info_dic['send_ID'], info_dic['send_PW'], info_dic['send_Pro'])
-    msg = setMsg(info_dic['receive_ID'], info_dic['send_title'], info_dic['send_cont'])
-    smtp.sendmail(info_dic['send_ID'], info_dic['receive_ID'], msg.as_string())
-    smtp.quit()
+class sender:
+    def __init__(self, id, password, provider):
+        self.id = id
+        self.smtp = smtplib.SMTP('smtp.' + provider, 587)
+        self.smtp.ehlo()
+        self.smtp.starttls()
+        self.smtp.login(id, password)
